c73c0127bd0f63af8488ade137eb3653e8b87da9
metter <metter@talionis.ch>
2021-02-06 11:25:12 +0100

    More work on the LevelView. Doesn't look good yet.

--- app/src/main/java/ch/talionis/rbx/views/LevelView.java
+++ app/src/main/java/ch/talionis/rbx/views/LevelView.java
@@ -2,10 +2,12 @@ package ch.talionis.rbx.views;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.LinearGradient;
 import android.graphics.Outline;
 import android.graphics.Paint;
 import android.graphics.Path;
-import android.graphics.Point;
+import android.graphics.Shader;
 import android.util.AttributeSet;
 import android.view.View;
 import android.view.ViewOutlineProvider;
@@ -18,6 +20,7 @@ import ch.talionis.rbx.R;
 
 import static android.graphics.Paint.ANTI_ALIAS_FLAG;
 import static android.graphics.Paint.Cap.ROUND;
+import static android.graphics.Paint.Style.FILL;
 import static android.graphics.Paint.Style.STROKE;
 import static ch.talionis.rbx.android.AndroidUtils.dpToPx;
 
@@ -26,6 +29,11 @@ public class LevelView extends FrameLayout {
     private Paint strokePaint;
     private float roundRectRadius;
     private int strokeWidth;
+    private int starRowHorizontalPadding;
+    private StarRow starRowView;
+    private Paint shaderPaint;
+    private Paint textPaint;
+    private String text = "1";
 
     public LevelView(@NonNull Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
@@ -44,15 +52,30 @@ public class LevelView extends FrameLayout {
         strokePaint.setStrokeWidth(strokeWidth);
         strokePaint.setColor(getResources().getColor(R.color.view_level_stroke));
 
+        shaderPaint = new Paint();
+        shaderPaint.setFlags(ANTI_ALIAS_FLAG);
+        shaderPaint.setStyle(FILL);
+
+        textPaint = new Paint();
+        textPaint.setFlags(ANTI_ALIAS_FLAG);
+        textPaint.setStrokeWidth(getResources().getDimension(R.dimen.view_pentagon_stroke_width));
+        textPaint.setColor(getResources().getColor(R.color.view_level_text_paint));
+        textPaint.setStyle(FILL);
+
         path = new Path();
 
         roundRectRadius = dpToPx(getContext(), 8);
+
+        starRowHorizontalPadding = (int) dpToPx(getContext(), 4);
+        starRowView = new StarRow(getContext(), null);
+        addView(starRowView);
     }
 
     @Override
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
         super.onSizeChanged(w, h, oldw, oldh);
         path = calculatePath(w, h);
+        shaderPaint.setShader(new LinearGradient(getWidth() / 2, 0, getWidth() / 2, getHeight(), getResources().getColor(R.color.view_level_gradient_top), getResources().getColor(R.color.view_level_gradient_bottom), Shader.TileMode.MIRROR));
         setOutlineProvider(new ViewOutlineProvider() {
             @Override
             public void getOutline(View view, Outline outline) {
@@ -62,19 +85,37 @@ public class LevelView extends FrameLayout {
     }
 
     @Override
+    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+        int width = right - left;
+        int height = bottom - top;
+
+        int starRowHeight = height / 3;
+
+        starRowView.layout(starRowHorizontalPadding, 2 * starRowHeight, width - starRowHorizontalPadding, 3 * starRowHeight);
+    }
+
+    @Override
     protected void onDraw(Canvas canvas) {
         if (path == null) {
             calculatePath(getWidth(), getHeight());
         }
 
+        canvas.drawPath(path, shaderPaint);
         canvas.drawPath(path, strokePaint);
+
+        if (text != null) {
+            textPaint.setTextSize(getHeight() / 2);
+            textPaint.setTextAlign(Paint.Align.CENTER);
+            canvas.drawText(text, getWidth() / 2, (int) ((getHeight() / 3) - ((textPaint.descent() + textPaint.ascent()) / 2)), textPaint);
+        }
+
+        super.onDraw(canvas);
     }
 
     private Path calculatePath(int width, int height) {
 
         Path path = new Path();
-        path.moveTo(strokeWidth, strokeWidth);
-        path.addRoundRect(strokeWidth, strokeWidth, width - strokeWidth - strokeWidth, height - strokeWidth - strokeWidth, roundRectRadius, roundRectRadius, Path.Direction.CW);
+        path.addRoundRect(strokeWidth, strokeWidth, width - strokeWidth, height - strokeWidth, roundRectRadius, roundRectRadius, Path.Direction.CW);
         return path;
     }
 }
